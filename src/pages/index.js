60ace79d15af616f6946311f37eba10b7dae86d5
Hugo Perdomo <hugo.j.dev@gmail.com>
2019-03-15 22:51:36 -0400

    Adding responsiveness to navcontainer and navitem components, also making use of the github icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,12 @@ import Content from '../components/Content'
 import styles from '../styles/icons.module.scss'
 
 export default () => (
-  <Layout className="video-main" transparent>
+  <Layout className='video-main' transparent>
     <video autoPlay muted loop id='video'>
       <source src='static/video/code_video.mp4' type='video/mp4' />
       Your browser does not support HTML5 video.
     </video>
-    <NavBar transparent/>
+    <NavBar transparent />
     <Content>
       <Title size={100} fw={600}>
         I Build Modern Web Apps with Tomorrow's Tools{' '}
@@ -23,7 +23,13 @@ export default () => (
         mean time you can check out my github and see all the cool stuff I'm
         currently working on:
       </h2>
-      
+      <a
+        href='https://github.com/p-hugo'
+        rel='noopener noreferrer'
+        target='_blank'
+      >
+        <FaGithub size='100px' className={styles.center} />
+      </a>
     </Content>
   </Layout>
 )
