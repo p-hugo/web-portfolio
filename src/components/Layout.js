a97899e535dc10079f82c4547ec006e180f28a8d
Hugo Perdomo <hugo.j.dev@gmail.com>
2019-03-20 21:26:43 -0400

    Made layout into a styled component and deleted its scss module counterpart

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import {layout, trans} from './layout.module.scss';
+import styled from 'styled-components'
 
-export default ({children, transparent}) => {
-    console.log(transparent);
-    return (
-        <div className={transparent ? `${layout} ${trans}`: layout}>{children}</div>
-    )
-};
+const Layout = styled.div`
+  margin: 0 auto;
+  border: none;
+  width: 100%;
+  background-color: ${props => props.transparent ? "rgba(23, 33, 45, 0.5)" : props.theme.main};
+  max-height: 100vh;
+  min-height: 100vh;
+  overflow: scroll;
+`
+
+Layout.defaultProps = {
+  theme: {
+    main: '#17212d'
+  }
+}
+
+export default Layout;
